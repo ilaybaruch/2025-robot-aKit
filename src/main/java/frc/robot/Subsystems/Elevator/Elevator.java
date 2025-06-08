9b6b98b0831d00680071fad2327091ed249a07b0
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2025-06-08 18:36:48 +0300

    started working on subsystem

--- src/main/java/frc/robot/Subsystems/Elevator/Elevator.java
+++ src/main/java/frc/robot/Subsystems/Elevator/Elevator.java
@@ -1,5 +1,18 @@
 package frc.robot.Subsystems.Elevator;
 
-public class Elevator {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase {
+    private final ElevatorIO elevatorIO;
+    private final ElevatorIOAutoLogged elevatorInputs = new ElevatorIOAutoLogged();
+
+    public Elevator(ElevatorIO elevatorIO) {
+        this.elevatorIO = elevatorIO;
+    }
+
+    public void periodic() {
+        elevatorIO.updateInputs(elevatorInputs);
+        Logger
+    }
 
 }
--- src/main/java/frc/robot/Subsystems/Elevator/ElevatorSparkMax.java
+++ src/main/java/frc/robot/Subsystems/Elevator/ElevatorSparkMax.java
@@ -1,48 +1,47 @@
 
-package frc.robot.Subsystems.Elevator;
+// package frc.robot.Subsystems.Elevator;
 
-import static frc.robot.Subsystems.Elevator.ElevatorConstants.*;
+// import static frc.robot.Subsystems.Elevator.ElevatorConstants.*;
 
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.config.SparkMaxConfig;
+// import com.revrobotics.CANSparkBase.IdleMode;
+// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+// import com.revrobotics.RelativeEncoder;
+// import com.revrobotics.spark.config.SparkMaxConf
 
+// import edu.wpi.first.math.controller.ElevatorFeedforward;
+// import edu.wpi.first.math.controller.ProfiledPIDController;
+// import edu.wpi.first.math.trajectory.TrapezoidProfile;
+// import edu.wpi.first.wpilibj.DigitalInput;
+// import frc.robot.POM_lib.Motors.POMSparkMax;
 
-import edu.wpi.first.math.controller.ElevatorFeedforward;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.DigitalInput;
-import frc.robot.POM_lib.Motors.POMSparkMax;
+// public class ElevatorSparkMax implements ElevatorIO {
 
-public class ElevatorSparkMax implements ElevatorIO {
+//     private final POMSparkMax motor = new POMSparkMax(0);
+//     private final RelativeEncoder encoder = motor.getEncoder();
+//     private final DigitalInput limitSwitch = new DigitalInput(0);
+//     ProfiledPIDController pidController;
+//     private ElevatorFeedforward feedforward;
 
-    private final POMSparkMax motor = new POMSparkMax(0);
-    private final RelativeEncoder encoder = motor.getEncoder();
-    private final DigitalInput limitSwitch = new DigitalInput(0);
-    ProfiledPIDController pidController;
-    private ElevatorFeedforward feedforward;
+//     public ElevatorSparkMax() {
 
-    public ElevatorSparkMax() {
+//         feedforward = new ElevatorFeedforward(Ks, Kg, Kv, Ka);
+//         pidController = new ProfiledPIDController(Kp, Ki, Kd,
+//                 new TrapezoidProfile.Constraints(MAX_VELOCITY, MAX_ACCELERATION));
 
-        feedforward = new ElevatorFeedforward(Ks, Kg, Kv, Ka);
-        pidController = new ProfiledPIDController(Kp, Ki, Kd,
-                new TrapezoidProfile.Constraints(MAX_VELOCITY, MAX_ACCELERATION));
+//         SparkMaxConfig config = new SparkMaxConfig();
 
-        SparkMaxConfig config = new SparkMaxConfig();
+//         config.idleMode(IdleMode.kBrake).inverted(INVERTED)
+//                 .smartCurrentLimit(CURRENT_LIMIT)
+//                 .voltageCompensation(VOLTAGE_COMPENSATION);
 
-        config.idleMode(IdleMode.kBrake).inverted(INVERTED)
-                .smartCurrentLimit(CURRENT_LIMIT)
-                .voltageCompensation(VOLTAGE_COMPENSATION);
+//         config.encoder.positionConversionFactor(POSITION_CONVERSION_FACTOR)
+//                 .velocityConversionFactor(POSITION_CONVERSION_FACTOR / 60.0);
 
-        config.encoder.positionConversionFactor(POSITION_CONVERSION_FACTOR)
-                .velocityConversionFactor(POSITION_CONVERSION_FACTOR / 60.0);
+//         motor.configure(config);
 
-        motor.configure(config);
+//         encoder.setPosition(0);
 
-        encoder.setPosition(0);
+//         pidController.setTolerance(0);
 
-        pidController.setTolerance(0);
-
-    }
-}
+//     }
+// }
